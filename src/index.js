998846c8a49b568c44bef91af78001b356944697
markprodigo <markprodz7pro@gmail.com>
2025-09-13 04:39:36 +0800

    Prepare for deployment

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import necessary dependencies
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Assuming your main component is named App and defined in App.js
+
+// Render the main component
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
